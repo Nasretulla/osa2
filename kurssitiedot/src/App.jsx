c40a2de0b73b3a8a5a51e8da5a60cb27abe50224
Nasretulla <folad8@hotmail.com>
2023-10-27 10:10:21 +0300

    2.1  kurssitiedot step 6

--- kurssitiedot/src/App.jsx
+++ kurssitiedot/src/App.jsx
@@ -1,34 +1,71 @@
-import { useState } from 'react'
 
+
+const Header = ({header}) => {
+  return (
+    
+      <h1> {header}  </h1>
+    
+  );
+};
+const Part = (props) => {
+  console.log(props.osat)
+  
+  return <>
+      <p>{props.osat.name} {props.osat.exercises}</p>
+      
+    
+  </>
+};
+
+const Content = (props) => {
+  console.log(props.sisalto);
+  return (
+    <>
+      <Part  osat={props.sisalto[0]} />
+      <Part osat={props.sisalto[1]} />
+      <Part osat={props.sisalto[2]} />
+    </>
+  );
+};
+const Course = (props) => {
+  console.log(props);
+  return (
+    <>
+      <Header header={props.kurssi.name} />
+      <Content  sisalto= {props.kurssi.parts}/>
+    </>
+  );
+};
 
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     id: 1,
     parts: [
       {
-        name: 'Fundamentals of React',
+        name: "Fundamentals of React",
         exercises: 10,
-        id: 1
+        id: 1,
       },
       {
-        name: 'Using props to pass data',
+        name: "Using props to pass data",
         exercises: 7,
-        id: 2
+        id: 2,
       },
       {
-        name: 'State of a component',
+        name: "State of a component",
         exercises: 14,
-        id: 3
-      }
-    ]
-  }
+        id: 3,
+      },
+    ],
+  };
 
   return (
-    <div>
-      <Course course={course} />
-    </div>
-  )
-}
-
-export default App
+    <>
+      <Course kurssi={course} />
+      
+      
+    </>
+  );
+};
+export default App;
